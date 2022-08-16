7fe5ea946edd7a71e91283835587fe9b3f50211a
jekq85 <30422479+jekq85@users.noreply.github.com>
2022-08-16 00:57:40 -0500

    Update V1709_1002__EPECUS_USERS.sql

--- src/db/ddl/V1709/V1709_1002__EPECUS_USERS.sql
+++ src/db/ddl/V1709/V1709_1002__EPECUS_USERS.sql
@@ -1,15 +1,15 @@
 CREATE TABLE epecus_users (
     user_seq_id     INTEGER NOT NULL,
-    user_id         VARCHAR2(50) NOT NULL,
-    user_cd         VARCHAR2(1),
-    status_cd       VARCHAR2(1),
-    user_name       VARCHAR2(50) NOT NULL,
-    user_email      VARCHAR2(100),
+    user_id         VARCHAR(50) NOT NULL,
+    user_cd         VARCHAR(1),
+    status_cd       VARCHAR(1),
+    user_name       VARCHAR(50) NOT NULL,
+    user_email      VARCHAR(100),
     login_cnt       INTEGER,
-    update_by       VARCHAR2(50),
+    update_by       VARCHAR(50),
     last_login_dt   DATE,
     update_dt       DATE,
-    create_by       VARCHAR2(50),
+    create_by       VARCHAR(50),
     create_dt       DATE
 )
-    LOGGING;
+;
