a9dbb470bde714ca0cadedd2fb8312643c6d46d7
jekq85 <30422479+jekq85@users.noreply.github.com>
2022-08-16 01:00:47 -0500

    Update V1709_1008__STOCK_MGR.sql

--- src/db/ddl/V1709/V1709_1008__STOCK_MGR.sql
+++ src/db/ddl/V1709/V1709_1008__STOCK_MGR.sql
@@ -1,11 +1,11 @@
 CREATE TABLE stock_mgr (
     stock_mgr_id   INTEGER NOT NULL,
-    stock          VARCHAR2(20),
-    corp_name      VARCHAR2(100),
-    freq_cd        VARCHAR2(10),
-    update_by      VARCHAR2(50),
+    stock          VARCHAR(20),
+    corp_name      VARCHAR(100),
+    freq_cd        VARCHAR(10),
+    update_by      VARCHAR(50),
     update_dt      DATE,
-    create_by      VARCHAR2(50),
+    create_by      VARCHAR(50),
     create_dt      DATE
 )
-    LOGGING;
+;
