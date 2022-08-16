1ad6c9b3ab3c6c7d7fd657b95be81d47a5b931c3
jekq85 <30422479+jekq85@users.noreply.github.com>
2022-08-16 00:58:21 -0500

    Update V1709_1004__FIN_DATA.sql

--- src/db/ddl/V1709/V1709_1004__FIN_DATA.sql
+++ src/db/ddl/V1709/V1709_1004__FIN_DATA.sql
@@ -1,12 +1,12 @@
 CREATE TABLE fin_data (
     fin_data_id   INTEGER NOT NULL,
-    stock         VARCHAR2(20),
-    fin_item      VARCHAR2(100),
-    minus_4yr     VARCHAR2(50),
-    minus_3yr     VARCHAR2(50),
-    minus_2yr     VARCHAR2(50),
-    minus_1yr     VARCHAR2(50),
+    stock         VARCHAR(20),
+    fin_item      VARCHAR(100),
+    minus_4yr     VARCHAR(50),
+    minus_3yr     VARCHAR(50),
+    minus_2yr     VARCHAR(50),
+    minus_1yr     VARCHAR(50),
     update_dt     DATE,
     create_dt     DATE
 )
-    LOGGING;
+;
