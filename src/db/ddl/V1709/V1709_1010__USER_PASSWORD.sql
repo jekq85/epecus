225e07976cbb15ebb17e46282a9bf0fa951096ac
jekq85 <30422479+jekq85@users.noreply.github.com>
2022-08-16 01:01:51 -0500

    Update V1709_1010__USER_PASSWORD.sql

--- src/db/ddl/V1709/V1709_1010__USER_PASSWORD.sql
+++ src/db/ddl/V1709/V1709_1010__USER_PASSWORD.sql
@@ -1,10 +1,9 @@
 CREATE TABLE user_password (
     password_seq_id   INTEGER NOT NULL,
     user_seq_id       INTEGER NOT NULL,
-    password          VARCHAR2(50) NOT NULL,
-    update_by         VARCHAR2(50),
+    password          VARCHAR(50) NOT NULL,
+    update_by         VARCHAR(50),
     update_dt         DATE,
-    create_by         VARCHAR2(50),
+    create_by         VARCHAR(50),
     create_dt         DATE
-)
-    LOGGING;
+);
